a47784c9b976c2b8d13dfa27833e62f27f7324c9
daclouds <daclouds@gmail.com>
2015-07-03 18:45:25 +0900

    Initial Commit

---
+++ Tview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tview
+//
+//  Created by 1001914 on 2015. 7. 3..
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
