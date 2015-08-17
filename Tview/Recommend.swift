0dc19d2789db880ad372b6320455f2912f808638
teuskim <kim5724@hanmail.net>
2015-08-17 19:57:45 +0900

    추천 관련

---
+++ Tview/Recommend.swift
@@ -0,0 +1,19 @@
+//
+//  Recommend.swift
+//  Tview
+//
+//  Created by 김태우 on 8/17/15.
+//
+//
+
+import UIKit
+
+class Recommend {
+    var title: String
+    var imageUrl: String
+    
+    init (title: String, imageUrl: String) {
+        self.title = title
+        self.imageUrl = imageUrl
+    }
+}
